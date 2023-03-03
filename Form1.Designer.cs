ae032a3e930d999d2f3bb443b1a8811a2579782d
a.kudaibergen <kudaibergen.rabota@gmail.com>
2023-03-03 15:28:29 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(75, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Калькулятор чего-то";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 163);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static double R = 1;
+        static double r1 = 0.45;
+        static double h1 = 0.33;
+        static double r2 = 0.25;
+        static double h2 = 2.57;
+        static double r3 = 1.5;
+        static double h3 = 0.8;
+        static double h4 = 0.7;
+        static double h5 = 0.35;
+        static double h6 = 6.07;
+        static double h7 = 1.22;
+        double v1 = (3.14 * (R * R + r1 * r1 + R * r1) * h1) / 3;
+        double v2 = 3.14 * R * R * h2;
+        double v3 = (3.14 * (R * R + r2 * r2 + R * r2) * h3) / 3;
+        double v4 = 3.14 * r2 * r2 * h4;
+        double v5 = (3.14 * (r3 * r3 + r2 * r2 + r2 * r3) * h5) / 3;
+        double v6 = 3.14 * r3 * r3 * h6;
+        double v7 = (3.14 * r3 * r3 * h7) / 3;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                double totalVolume = v1 + v2 + v3 + v4 + v5 + v6 + v7;
+                double h = double.Parse(textBox1.Text.ToString().Replace(".", ","));
+                textBox1.Clear();
+                textBox1.Focus();
+                double result = totalVolume - calCulateEmptyVolume(h);
+                textBox1.Text = result.ToString();
+            }
+            catch (Exception ex) {
+                textBox1.Text = "ERROR";
+            }
+        }
+
+        private double calCulateEmptyVolume(double h)
+        {
+            double emptyValume = 0;
+            if (h > h1)
+            {
+                emptyValume += v1;
+                h = h - h1;
+            }
+            else
+            {
+               return (3.14 * (R * R + r1 * r1 + R * r1) * h) / 3;
+            }
+
+            if (h > h2)
+            {
+                emptyValume += v2;
+                h = h - h2;
+            }
+            else
+            {
+                return emptyValume += 3.14 * R * R * h;
+            }
+
+            if (h > h3)
+            {
+                emptyValume += v3;
+                h = h - h3;
+            }
+            else
+            {
+                return emptyValume += (3.14 * (R * R + r2 * r2 + R * r2) * h) / 3;
+            }
+
+            if (h > h4)
+            {
+                emptyValume += v4;
+                h = h - h4;
+            }
+            else
+            {
+                return emptyValume += 3.14 * r2 * r2 * h;
+            }
+
+            if (h > h5)
+            {
+                emptyValume += v5;
+                h = h - h5;
+            }
+            else
+            {
+                return emptyValume += (3.14 * (r3 * r3 + r2 * r2 + r2 * r3) * h) / 3;
+            }
+
+            if (h > h6)
+            {
+                emptyValume += v6;
+                h = h - h6;
+            }
+            else
+            {
+                return emptyValume += 3.14 * r3 * r3 * h6;
+            }
+
+            if (h > h7)
+            {
+                emptyValume += v7;
+                h = h - h7;
+            }
+            else
+            {
+                return emptyValume += (3.14 * r3 * r3 * h7) / 3;
+            }
+
+            return emptyValume;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+    }
+}
